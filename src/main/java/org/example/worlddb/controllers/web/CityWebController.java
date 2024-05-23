81b79b0c9e19817f91aab3c05e8d17c73a37a08e
Daniell <dbow@spartaglobal.com>
2024-05-23 13:07:05 +0100

    running branch

--- src/main/java/org/example/worlddb/controllers/web/CityWebController.java
+++ src/main/java/org/example/worlddb/controllers/web/CityWebController.java
@@ -1,25 +1,28 @@
 package org.example.worlddb.controllers.web;
 
 import org.example.worlddb.model.entities.CityEntity;
+import org.example.worlddb.model.repositories.CityEntityRepository;
 import org.example.worlddb.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/web/city")
+@RequestMapping("/web/cities")
 public class CityWebController {
 
 
     private final CityService cityService;
+    private final CityEntityRepository cityEntityRepository;
 
     @Autowired
-    public CityWebController(CityService cityService) {
+    public CityWebController(CityService cityService,
+                             CityEntityRepository cityEntityRepository) {
         this.cityService = cityService;
+        this.cityEntityRepository = cityEntityRepository;
     }
 
     @GetMapping("/")
@@ -28,9 +31,48 @@ public class CityWebController {
         return "cities";
     }
 
+    //
+    @GetMapping("/header")
+    public String getHeaders(Model model) {
+        return "index";
+    }
+
 
     private List<CityEntity> get10Cities(List<CityEntity> cities) {
         return cities.subList(0, 10);
     }
+
+    @GetMapping("/add-city")
+    public String addCity(Model model) {
+        CityEntityRepository cityEntityRepository1 = cityEntityRepository;
+        model.addAttribute("city", cityEntityRepository1);
+        return "add-city";
+    }
+    /* @GetMapping("/save-city")
+    public String saveCity(Model model) {}*/
+
+    @GetMapping("/delete/{id}")
+    public String deleteCityById(@PathVariable Integer id, Model model) {
+    cityService.getCityById(id).ifPresent(cityToDelete->cityService.deleteCity(id));
+        return "redirect:/web/cities";
+        }
+
+
+    @GetMapping("/update/{id}")
+    public String editCityById(@PathVariable Integer id, Model model) {
+        CityEntity cityToUpdate = cityService.getCityById(id).orElse(null);
+        model.addAttribute("city", cityToUpdate);
+        model.addAttribute("id", id);
+        return "update-city";
+    }
+
+    @PostMapping("/update-city/{id}")
+    public String updateCity(@PathVariable Integer id, @ModelAttribute("city")  CityService cityService) {
+    CityEntity cityToUpdate = cityService.getCityById(id).orElse(null);
+    return "redirect:/web/cities";
+    }
+
+
+
 }
 
--- src/main/java/org/example/worlddb/controllers/web/CountryWebController.java
+++ src/main/java/org/example/worlddb/controllers/web/CountryWebController.java
@@ -1,19 +1,25 @@
 package org.example.worlddb.controllers.web;
 
+import org.example.worlddb.model.entities.CountryEntity;
+import org.example.worlddb.model.repositories.CountryEntityRepository;
 import org.example.worlddb.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CountryWebController {
     private final CountryService countryService;
+    private final CountryEntityRepository countryEntityRepository;
 
     @Autowired
-    public CountryWebController(CountryService countryService) {
+    public CountryWebController(CountryService countryService, CountryEntityRepository countryEntityRepository) {
         this.countryService = countryService;
+        this.countryEntityRepository = countryEntityRepository;
     }
 
     @GetMapping("/web/countries")
@@ -24,7 +30,51 @@ public class CountryWebController {
 
     @GetMapping("/web/country/{code}")
     public String getCountry(@PathVariable String code, Model model) {
-        model.addAttribute("country", countryService.getCountryByCode(code));
+        model.addAttribute("country", countryService.getCountryByCode(code).orElse(null));
         return "country";
     }
+
+    @GetMapping("/web/country/delete/{code}")
+    public String deleteCountry(@PathVariable String code) {
+        CountryEntity countryToDelete = countryService.getCountryByCode(code).orElse(null);
+        if (countryToDelete != null) {
+            countryEntityRepository.delete(countryToDelete);
+        }
+        return "redirect:/web/countries";
+    }
+
+    @GetMapping("/web/add-country")
+    public String addBook(Model model) {
+        CountryEntity country = new CountryEntity();
+        model.addAttribute("country", country);
+        return "add-country";
+    }
+    @PostMapping("/web/save-country")
+    public String saveCountry(@ModelAttribute("country") CountryEntity country) {
+        countryEntityRepository.save(country);
+        return "redirect:/web/countries";
+    }
+
+    @PostMapping("/web/country/update/{code}")
+    public String updateCountry(@PathVariable String code,String newName,String newContinent,String newRegion, Integer newCapital) {
+        CountryEntity updatedCountry = new CountryEntity();
+       updatedCountry.setName(newName);
+       updatedCountry.setContinent(newContinent);
+       updatedCountry.setRegion(newRegion);
+       updatedCountry.setSurfaceArea(updatedCountry.getSurfaceArea());
+       updatedCountry.setIndepYear(updatedCountry.getIndepYear());
+       updatedCountry.setPopulation(updatedCountry.getPopulation());
+       updatedCountry.setLifeExpectancy(updatedCountry.getLifeExpectancy());
+       updatedCountry.setGnp(updatedCountry.getGnp());
+       updatedCountry.setGNPOld(updatedCountry.getGNPOld());
+       updatedCountry.setLocalName(updatedCountry.getLocalName());
+       updatedCountry.setGovernmentForm(updatedCountry.getGovernmentForm());
+       updatedCountry.setHeadOfState(updatedCountry.getHeadOfState());
+       updatedCountry.setCapital(updatedCountry.getCapital());
+       updatedCountry.setCode2(updatedCountry.getCode2());
+        countryService.updateCountry(code, updatedCountry);
+        return "redirect:/web/countries";
+    }
+
+
 }
