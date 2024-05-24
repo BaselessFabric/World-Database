17072d30a379afbdd6ea173fd0b5209d594a9b7d
Peatruck <patrickmassonward@gmail.com>
2024-05-24 11:02:33 +0100

    attempted bug fixing

--- src/main/java/org/example/worlddb/WorldDbApplication.java
+++ src/main/java/org/example/worlddb/WorldDbApplication.java
@@ -1,6 +1,7 @@
 package org.example.worlddb;
 
 import org.example.worlddb.model.repositories.CountryLanguageEntityRepository;
+import org.example.worlddb.service.CountryLanguageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +17,14 @@ public class WorldDbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorldDbApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(CountryLanguageService countryLanguageService) {
+        return args -> {
+          System.out.println("Country Language: " + countryLanguageService.getCountryLanguageById("ABW", "English").get());
+          System.out.println("\n");
+        };
     }
 
 }
